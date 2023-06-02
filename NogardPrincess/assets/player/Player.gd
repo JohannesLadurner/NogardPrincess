449cb0d8f17f21efc12f5e351e36fbeb086fd921
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2023-06-02 21:57:11 +0200

    adjusted gravity

--- NogardPrincess/assets/player/Player.gd
+++ NogardPrincess/assets/player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 export var move_speed := 100
-export var jump_speed := 400
-export var gravity := 1500
+export var jump_speed := 350
+export var gravity := 1000
 var velocity := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
@@ -25,3 +25,20 @@ func _physics_process(delta: float) -> void:
 
 	# actually move the player
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+func _process(delta: float) -> void:
+	change_animation()
+
+func change_animation():
+	# face left or right
+	if velocity.x > 0:
+		$AnimatedSprite.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite.flip_h = true
+	if velocity.y < 0: # negative Y is up
+		$AnimatedSprite.play("jump")
+	else:
+		if velocity.x != 0:
+			$AnimatedSprite.play("walk")
+		else:
+			$AnimatedSprite.play("idle")
