5872f9d3fe77e46b608775485ef6e2a0bc3371e5
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2023-06-03 12:16:53 +0200

    added enemy awakening

--- NogardPrincess/assets/enemies/Enemy.gd
+++ NogardPrincess/assets/enemies/Enemy.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 onready var player = get_parent().get_node("Player")
 var move_speed = 50
-export var gravity := 1000
+export var gravity := 3000
 var velocity := Vector2.ZERO
 var player_position
 export var triggered = false
@@ -13,25 +13,35 @@ var readyToAttack = true
 
 func _ready():
 	$AnimatedSprite.play("dead")
+	$AnimatedSprite.connect("animation_finished", self, "animation_finished")
 	attack_timer.connect("timeout",self,"attack_delay_resetted")
 	attack_timer.wait_time = 3
 	attack_timer.one_shot = true
 	add_child(attack_timer)
 
 func _process(delta):
+	if $AnimatedSprite.animation == "awake":
+		return
+		
 	player_position = player.position
 	var target_position = (player.position - position).normalized()
 	if triggered or position.distance_to(player.position) < 50:
 		move_and_slide(target_position * move_speed)
-		triggered = true
+		if triggered == false:
+			$AnimatedSprite.play("awake")
+			triggered = true
+			return
 	
+	if $AnimatedSprite.animation == "dead":
+		return
+		
 	if player_position.x > position.x:
 		$AnimatedSprite.flip_h = true
 	else:
 		$AnimatedSprite.flip_h = false
 	
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity = move_and_slide(velocity, Vector2.ZERO)
 	
 	if position.distance_to(player.position) < 20 and readyToAttack:
 		attack()
@@ -44,3 +54,7 @@ func attack():
 	attack_timer.start()
 	player.health += 1
 	readyToAttack = false
+	
+func animation_finished():
+	if $AnimatedSprite.animation == "awake":
+		$AnimatedSprite.play("default")
