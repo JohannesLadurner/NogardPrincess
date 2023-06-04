e7e8641cf79e9931d7d4148ec20b33cec96d6a94
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2023-06-04 13:15:28 +0200

    fixed norgard

--- NogardPrincess/assets/enemies/nogard/Nogard.gd
+++ NogardPrincess/assets/enemies/nogard/Nogard.gd
@@ -1,18 +1,29 @@
 extends KinematicBody2D
 
-var health = 0
+onready var player = get_parent().get_node("Player")
+export var triggered = false
+
 func _ready():
 	$AnimatedSprite.play("dead")
+	$AnimatedSprite.connect("animation_finished", self, "animation_finished")
 	if !GlobalProperties.is_reverse:
-		$CollisionShape2D.disabled = true
 		$Area2D/CollisionShape2D.disabled = true
 		
 	
 func _process(delta):
 	if !GlobalProperties.is_reverse:
-		if health >= 10:
-			$AnimatedSprite.play("idle")
+		return
+	
+	if $AnimatedSprite.animation == "awake":
+		return
+	
+	if position.x > player.position.x + 50 and !triggered:
+		print("awake")
+		$AnimatedSprite.play("awake")
+		triggered = true
+	
 
-func get_damage():
-	if !GlobalProperties.is_reverse:
-		health += 1
+func animation_finished():
+	if $AnimatedSprite.animation == "awake":
+		$AnimatedSprite.play("idle")
+
