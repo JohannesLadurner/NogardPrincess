671ff07609435a371f3052d535384f7f562338a4
Lukas-12 <lukas.buchsi@gmail.com>
2023-06-04 14:38:44 +0200

    chest sound

--- NogardPrincess/assets/chests/Chest.gd
+++ NogardPrincess/assets/chests/Chest.gd
@@ -1,17 +1,20 @@
 extends AnimatedSprite
 
 onready var player = get_parent().get_node("Player")
-
+var triggered = false
 func _ready():
 	play("closed")
 	$AnimatedSprite.visible = false
 	$AnimatedSprite.connect("animation_finished", self, "animation_finished")
 
 func _process(delta):
-	if position.distance_to(player.position) < 10:
+	if position.distance_to(player.position) < 10 and !triggered:
+		$AnimatedSprite.visible = true
 		$AnimatedSprite.play("blink")
 		GlobalProperties.next_skin()
+		$ChestSound.play()
 		play("opened")
+		triggered = true
 
 func animation_finished():
 	$AnimatedSprite.visible = false
